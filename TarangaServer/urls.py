9c4e3e3c50bfdbb02899000d45dd500d44b5fff2
Nabin Adhikari <36256500+NabinAdhikari674@users.noreply.github.com>
2021-07-21 22:09:19 +0545

    Updated Various URLs for various endpoints

--- TarangaServer/urls.py
+++ TarangaServer/urls.py
@@ -5,5 +5,12 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('stockInfo/', views.stockInfo, name = "stockInfo"),
-    path('marketStatus/', views.marketStatus, name = "marketStatus")
+    path('marketStatus/', views.marketStatus, name = "marketStatus"),
+    path('notice/',views.notice, name = "notice"),
+    path('disclosure/',views.disclosure, name = "disclosure"),
+    path('nepseIndex/',views.nepseIndex, name = "nepseIndex"),
+    path('marketSummary/',views.marketSummary, name = "marketSummary"),
+    path('topGainers/',views.topGainers, name = "topGainers"),
+    path('topTurnovers/',views.topTurnovers, name = "topTurnovers"),
+    path('supplyDemand/',views.supplyDemand, name = "supplyDemand")
 ]
