9ff8862182a119375d562c9226bfc234efffee72
Nabin Adhikari <36256500+NabinAdhikari674@users.noreply.github.com>
2021-07-21 21:50:56 +0545

    Added API endpoint to check market status
    Can check market status with new API endpoint /marketStatus

--- TarangaServer/views.py
+++ TarangaServer/views.py
@@ -7,3 +7,10 @@ def stockInfo(request):
         header = Headers(headers=True)
         result = requests.get("https://newweb.nepalstock.com.np/api/nots/securityDailyTradeStat/58",headers=header.generate())
         return JsonResponse({'stockInfo':result.json()})
+
+def marketStatus(request):
+    #https://newweb.nepalstock.com.np/api/nots/nepse-data/market-open
+    if(request.method == "GET"):
+        header = Headers(headers=True)
+        result = requests.get("https://newweb.nepalstock.com.np/api/nots/nepse-data/market-open",headers=header.generate())
+        return JsonResponse({'marketStatus':result.json()})
